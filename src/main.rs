873c5140a67ab90f246aee93453c96eb4649964d
Paul Woolcock <paul@woolcock.us>
2016-06-13 16:40:16 -0400

    don't actually need the router to show this error, so simplify the code

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,17 @@
 #[macro_use]
 extern crate nickel;
 
-use nickel::{Nickel, HttpRouter, Router};
+use nickel::{Nickel, HttpRouter};
 
 struct ServerData;
 
 fn main() {
     let mut server = Nickel::with_data(ServerData);
 
-    server.utilize(explicit_router());
+    server.get("/",
+               middleware! { |_, res| <ServerData>
+                return res.send("foo")
+    });
 
     server.listen("0.0.0.0:0");
-}
-
-fn explicit_router() -> Router<ServerData> {
-    let mut router = Nickel::router();
-
-    router.get("/",
-               middleware! { |_, res| <ServerData>
-                 // if you change this to just:
-                 //
-                 // res.send("foo")
-                 //
-                 // it will not compile
-                return res.send("foo")
-            });
-
-    router
 }
